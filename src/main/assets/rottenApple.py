7693f7ae0d9835f524420ceba7ff3631944c51da
Rubivilo <monito.r@live.com>
2024-03-09 18:57:56 +0100

    added rotten apple from consumables

---
+++ src/main/assets/rottenApple.py
@@ -0,0 +1,7 @@
+import os
+from config import GRAPHICS_DIR
+from assets.consumable import Consumable 
+
+class RottenApple(Consumable):
+    def __init__(self, staticPositions):
+        super().__init__(os.path.join(GRAPHICS_DIR, 'apple_rotten.png'), staticPositions)
